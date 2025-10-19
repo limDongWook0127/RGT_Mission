b8b9bc823d9be8b27fafbddf60204fcf22b75bd4
DESKTOP-E1RKP31\que_d <que_d@naver.com>
2025-10-20 08:55:20 +0900

    feat: add custom Iterator for CircularBuffer

--- RGT_Mission2/CircularBuffer.h
+++ RGT_Mission2/CircularBuffer.h
@@ -63,7 +63,7 @@ public:
     {
         return _buffer[_head];
     }
-    
+
     const T &back() const
     {
         return _buffer[(_tail + _capacity - 1) % _capacity];
@@ -76,4 +76,42 @@ public:
             std::cout << _buffer[i] << " ";
         std::cout << "\n";
     }
+
+    class Iterator
+{
+private:
+	CircularBuffer* _parent;
+	size_t _index;
+	size_t _remaining;
+
+public:
+	Iterator(CircularBuffer* parent, size_t index, size_t remaining)
+		:_parent(parent), _index(index), _remaining(remaining)
+	{
+	}
+	// STL 호환을 위한 필수 typedefs
+	using iterator_category = std::forward_iterator_tag;  // 반복자 카테고리
+	using value_type = T;                                // 값 타입
+	using difference_type = std::ptrdiff_t;               // 거리 계산용 정수형
+	using pointer = T*;                                   // 포인터 타입
+	using reference = T&;                                 // 참조 타입
+
+	T& operator*()
+	{
+		return _parent->_buffer[_index];
+	}
+	Iterator& operator++()
+	{
+		_index = (_index + 1) % _parent->_capacity;
+		_remaining--;
+		return *this;
+	}
+	bool operator!=(const Iterator& other)
+	{
+		return _remaining != other._remaining;
+	}
+};
+
+Iterator begin() { return Iterator(this, _head, _size); }
+Iterator end() { return Iterator(this, _tail, 0); }
 };
